a159c7bdd351f2237a53e77269c2fdcdb56472dd
rivkicobersy <rcobersy@gmail.com>
2022-11-06 19:03:43 +0200

    signup (create new user) added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true
 
   has_many :pantry_items
   has_many :ingredients, through: :pantry_items
