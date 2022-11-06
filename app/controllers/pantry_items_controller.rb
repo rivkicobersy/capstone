298b58d632c41a28a08bf2987f90d583eb1fdd0a
rivkicobersy <rcobersy@gmail.com>
2022-11-06 20:29:48 +0200

    custom pantry items json

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,15 +1,15 @@
 class PantryItemsController < ApplicationController
   def index
-    pantry_items = PantryItem.all
-    render json: pantry_items.as_json
+    @pantry_items = PantryItem.all
+    render template: "pantry_items/index"
   end
 
   def create
-    pantry_item = PantryItem.create(
+    @pantry_item = PantryItem.create(
       ingredient_id: params[:ingredient_id],
       user_id: params[:user_id],
       amount: params[:amount],
     )
-    render json: pantry_item.as_json
+    render json: @pantry_item.as_json
   end
 end
